eedd3ea07f23d4021ce7a87f10d9b19ab224ccab
DuoPan <pd54007@gmail.com>
2018-04-11 16:29:24 +1000

    add search by date function

--- map_all.js
+++ map_all.js
@@ -6,31 +6,35 @@ var customLabel = {
     bar: {
       label: 'B'
     }
-    };
-    var hb1 = [
-['Humpback whale','Humpback whales are one of the most common species of whale you will see in Australian waters and are well known for their spectacular breaching behaviour. '],
-['Common dolphin','Common Dolphins are slender, with a long beak protruding sharply from the head. The dorsal fin is high and curves backwards.'],
-['Australian humpback dolphin','Australian humpback dolphins are usually grey with various white scarring and dark flecking in some areas. '],
-['Australian snubfin dolphin','The Australian snubfin dolphin is characterised by a broadly rounded head that is extremely mobile and usually has a visible neck crease. '],
-['Blue whale','Blue whales are the largest animal on earth with an average length of 24 meters and a weight of up to 136 tonnes.'],
-['Common bottlenose dolphin','The common bottlenose dolphin is grey in colour and grows to between 2 and 4 meters long. It has a short rounded snout, described as bottle-shaped.'],
-['Dwarf minke whale','The dwarf minke whale is the smallest baleen (filter feeding) whale, reaching less than 8m long.'],
-['Killer whale','Killer whales are the largest member of the dolphin family and are recognisable by their distinctive black, white and grey colouration. '],
-['Southern right whale','The southern right whale is a large whale that is easily distinguished from others because of its broad back without a dorsal fin, and wide pectoral fins'],
-['Spinner dolphin','The spinner dolphin is a slender dolphin with an extremely long, thin beak. The dorsal fin ranges from slightly sickle-shaped to being erect and triangular.']];
+};
+var hb1 = [
+  ['Humpback whale','Humpback whales are one of the most common species of whale you will see in Australian waters and are well known for their spectacular breaching behaviour. '],
+  ['Common dolphin','Common Dolphins are slender, with a long beak protruding sharply from the head. The dorsal fin is high and curves backwards.'],
+  ['Australian humpback dolphin','Australian humpback dolphins are usually grey with various white scarring and dark flecking in some areas. '],
+  ['Australian snubfin dolphin','The Australian snubfin dolphin is characterised by a broadly rounded head that is extremely mobile and usually has a visible neck crease. '],
+  ['Blue whale','Blue whales are the largest animal on earth with an average length of 24 meters and a weight of up to 136 tonnes.'],
+  ['Common bottlenose dolphin','The common bottlenose dolphin is grey in colour and grows to between 2 and 4 meters long. It has a short rounded snout, described as bottle-shaped.'],
+  ['Dwarf minke whale','The dwarf minke whale is the smallest baleen (filter feeding) whale, reaching less than 8m long.'],
+  ['Killer whale','Killer whales are the largest member of the dolphin family and are recognisable by their distinctive black, white and grey colouration. '],
+  ['Southern right whale','The southern right whale is a large whale that is easily distinguished from others because of its broad back without a dorsal fin, and wide pectoral fins'],
+  ['Spinner dolphin','The spinner dolphin is a slender dolphin with an extremely long, thin beak. The dorsal fin ranges from slightly sickle-shaped to being erect and triangular.']
+];
 
-    function initMap() {
-      map = new google.maps.Map(document.getElementById('map'), {
-      center: new google.maps.LatLng(-25.2744, 133.7751),
-      zoom: 4,
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
- //     scrollwheel: false
-    });
-    
-    var imgLoc = './assets/images/smallDesc/'
-    var infoWindow = new google.maps.InfoWindow;
-      //Reading Php File
-    downloadUrl('./Db_Connect.php', function(data) {
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+  center: new google.maps.LatLng(-25.2744, 133.7751),
+  zoom: 4,
+  mapTypeId: google.maps.MapTypeId.ROADMAP,
+  //     scrollwheel: false
+  });
+
+  var imgLoc = './assets/images/smallDesc/'
+  var infoWindow = new google.maps.InfoWindow;
+
+  showCurrentLocation(map);
+  
+  //Reading Php File
+  downloadUrl('./Db_Connect.php', function(data) {
       var xml = data.responseXML;
       var markers = xml.documentElement.getElementsByTagName('marker');
       Array.prototype.forEach.call(markers, function(markerElem) {
@@ -75,10 +79,10 @@ var customLabel = {
         
         });
         markerCluster = new MarkerClusterer(map,allmarkers,{imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
-      });
-      }
+  });
+}
     
-    function downloadUrl(url, callback) {
+function downloadUrl(url, callback) {
     var request = window.ActiveXObject ?
       new ActiveXObject('Microsoft.XMLHTTP') :
       new XMLHttpRequest;
@@ -92,5 +96,29 @@ var customLabel = {
 
     request.open('GET', url, true);
     request.send(null);
-    }
-    function doNothing(){}
+}
+
+function doNothing(){}
+
+function showCurrentLocation(map) {
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      image1 = "./assets/images/location1.png";
+      var marker = new google.maps.Marker({
+        position: pos,
+        icon: image1,
+      });
+      marker.setMap(map);
+    }, function() {
+      //handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    //handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
--- map_search.js
+++ map_search.js
@@ -1,16 +1,44 @@
 function search() {
-    var toSearch = $('#search-select-name').val()
-    if(toSearch === null)
+    document.getElementById('date-alert').classList.add("hidden");
+
+    var nameSearch = $('#search-select-name').val()
+    var dateStart = Date.parse(document.getElementById('date-start-input').value);
+    var dateEnd = Date.parse(document.getElementById('date-end-input').value);
+    // console.log(dateStart);
+    // console.log(dateEnd);
+    if(nameSearch === null)
     {
-        document.getElementById('search-select-name-label').innerHTML='Please select at least one name';
-        document.getElementById('search-select-name-label').style="color:red";
+        nameSearch=["allnames"]; 
+        //document.getElementById('search-select-name-label').innerHTML='Please select at least one name';
+        //document.getElementById('search-select-name-label').style="color:red";
     }
-    else
+    if(!dateStart)
     {
-        document.getElementById('search-select-name-label').innerHTML='Names:';
-        document.getElementById('search-select-name-label').style="color:default";
-        search_advance(toSearch);
+        dateStart = Date.parse('1999-12-31');
     }
+    if(!dateEnd)
+    {
+        var today = new Date();
+        var dd = today.getDate();
+        var yyyy = today.getFullYear();
+        var mm = today.getMonth()+1; //January is 0!
+        if(dd<10) {
+            dd = '0'+dd;
+        }
+        if(mm<10) {
+            mm = '0'+mm
+        } 
+        today = yyyy + '-' + mm + '-' + dd;
+        dateEnd = Date.parse(today);
+    }
+    if(!verifyDate(dateStart, dateEnd))
+    {
+        document.getElementById('date-alert').classList.remove("hidden");
+        return;
+    }
+    
+
+    search_advance(nameSearch, dateStart, dateEnd);
 }
 
 function clearMap() {
@@ -29,7 +57,7 @@ function recenter() {
     map.setZoom(4);
 }
 
-function search_advance(toSearch)
+function search_advance(nameSearch, dateStart, dateEnd)
 {
     clearMap();
     var imgLoc = './assets/images/smallDesc/'
@@ -55,6 +83,23 @@ function search_advance(toSearch)
             //infowincontent.appendChild(document.createElement('br'));
             //var text = document.createElement('text');
             //infowincontent.appendChild(text);  
+            
+            if(nameSearch[0]!=="allnames")
+            {
+                var isNameExist = false;
+                nameSearch.forEach(function(element) {
+                    if(name === element){
+                        isNameExist = true;
+                    }
+                }); 
+                if(!isNameExist)
+                    return;
+            }
+            var thisDate = trasformYear(year);
+            if(thisDate < dateStart || thisDate > dateEnd)
+            {
+                return;
+            }
 
             var marker = new google.maps.Marker({ position: point, icon: './assets/images/whale.png' });
             google.maps.event.addListener(marker, 'click', function (evt) {
@@ -75,13 +120,43 @@ function search_advance(toSearch)
                     }
                 }
             });
-            toSearch.forEach(function(element) {
-                if(name === element){
-                    allmarkers.push(marker);
-                }
-            }); 
+            allmarkers.push(marker);
+
+            // if(nameSearch[0]!=="allnames")
+            // {
+            //     nameSearch.forEach(function(element) {
+            //         if(name === element){
+            //             allmarkers.push(marker);
+            //         }
+            //     }); 
+            // }
+            // else // search for all whales
+            // {
+            //     allmarkers.push(marker);
+            // }
+            
         });
         markerCluster = new MarkerClusterer(map, allmarkers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
     });
     setTimeout(function(){ recenter(); }, 200);
+}
+
+function verifyDate(dateStart, dateEnd)
+{
+    if(dateStart < dateEnd)
+        return true;
+    else
+        return false;
+}
+
+function trasformYear(year)
+{
+    var theDate = year.split('/');
+    if(theDate[0]<10) {
+        theDate[0] = '0'+theDate[0];
+    }
+    if(theDate[1]<10) {
+        theDate[1] = '0'+theDate[1];
+    } 
+    return Date.parse(theDate[2] + '-' + theDate[1] + '-' + theDate[0]);
 }
