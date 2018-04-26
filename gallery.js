f84054d9f715061237997973754aafb44c463d6d
DuoPan <pd54007@gmail.com>
2018-04-26 14:17:15 +1000

    Add nav page function.

--- gallery.js
+++ gallery.js
@@ -3,7 +3,17 @@ page.innerHTML = 1;
 var filenames = [];
 
 displayPage(1);
-
+var countImages = 0;
+var countPages = 0;
+loadTotalNum('./DB_Gallery_Count.php', function(data) {
+	var xml = data.responseXML;
+	var markers = xml.documentElement.getElementsByTagName('marker');
+	countImages = parseInt(markers[0].getAttribute('total'));
+	document.getElementById('destotal').innerHTML = countImages;
+	countPages = Math.ceil(countImages/12)
+	document.getElementById('navtotal').innerHTML = countPages;
+	document.getElementById('navtotal2').innerHTML = countPages;
+});
 
 function loadImages(url, page, callback) {
     var request = window.ActiveXObject ?
@@ -16,6 +26,20 @@ function loadImages(url, page, callback) {
       }
     };
     request.open('GET', url+"?page="+page, true);
+    request.send(null);
+}
+
+function loadTotalNum(url, callback) {
+    var request = window.ActiveXObject ?
+      new ActiveXObject('Microsoft.XMLHTTP') :
+      new XMLHttpRequest;
+    request.onreadystatechange = function() {
+      if (request.readyState == 4) {
+      request.onreadystatechange = doNothing;
+      callback(request, request.status);
+      }
+    };
+    request.open('GET', url, true);
     request.send(null);
 }
 
@@ -57,36 +81,31 @@ function displayPage(pageNum) {
             div5.appendChild(p);
         });
         baguetteBox.run('.tz-gallery');
-        if(filenames.length < 12) {
-            document.getElementById('nextButton').disabled = true;
-        }
-        else {
-            document.getElementById('nextButton').disabled = false;
-        }
-        if(page.innerHTML === '1') {
-            document.getElementById('previousButton').disabled = true;
-        }
-        else {
-            document.getElementById('previousButton').disabled = false;
-        }
-        
     });
 }
 
 function nextPage() {
-    page.innerHTML = parseInt(page.innerHTML) + 1;
-    var mainDiv = document.getElementById('putpics');
-    while (mainDiv.firstChild) {
-        mainDiv.removeChild(mainDiv.firstChild);
-    }
-    displayPage(parseInt(page.innerHTML));
+	if (parseInt(document.getElementById('currPage').innerHTML) === countPages) {
+		return;		
+	}
+	page.innerHTML = parseInt(page.innerHTML) + 1;
+	document.getElementById('currPage2').innerHTML =	page.innerHTML;
+	var mainDiv = document.getElementById('putpics');
+	while (mainDiv.firstChild) {
+			mainDiv.removeChild(mainDiv.firstChild);
+	}
+	displayPage(parseInt(page.innerHTML));
 }
 
 function previousPage() {
-    page.innerHTML = parseInt(page.innerHTML) - 1;
-    var mainDiv = document.getElementById('putpics');
-    while (mainDiv.firstChild) {
-        mainDiv.removeChild(mainDiv.firstChild);
-    }
-    displayPage(parseInt(page.innerHTML));
+	if (parseInt(document.getElementById('currPage').innerHTML) === 1) {
+		return;		
+	}
+	page.innerHTML = parseInt(page.innerHTML) - 1;
+	document.getElementById('currPage2').innerHTML =	page.innerHTML;
+	var mainDiv = document.getElementById('putpics');
+	while (mainDiv.firstChild) {
+			mainDiv.removeChild(mainDiv.firstChild);
+	}
+	displayPage(parseInt(page.innerHTML));
 }
