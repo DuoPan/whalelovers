bf7527e2e0409919f4ace4416299e87390b4d91b
DuoPan <pd54007@gmail.com>
2018-04-26 23:53:11 +1000

    Add upload preview function.

--- upload.php
+++ upload.php
@@ -1,4 +1,5 @@
 <?php
+    $isUploaded = false;
     $pic = $_FILES['pic']['tmp_name'];
     if($pic){
         $uploadDir = 'assets/photos'; 
@@ -6,6 +7,15 @@
             mkdir($uploadDir, 0777);    
         }    
         $targetFile = $uploadDir . '/' . time() . $_FILES['pic']['name'];
-        move_uploaded_file($pic, $targetFile) ? true : false;
+        $isUploaded = move_uploaded_file($pic, $targetFile) ? true : false;
     }
+
+    if($isUploaded)
+    {
+        echo 'true';
+    }
+    else{
+        echo 'false';
+    }
+    
 ?>
